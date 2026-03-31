d314fd67171bdf78e6d1ff4c32592a189cd7ed80
ffsaenterprises <159795776+ffsaenterprises@users.noreply.github.com>
2026-03-31 08:31:53 -0500

    g

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-global.__isHosted = process.env["CLOUDBOOST_HOSTED"] || false
-global.TUTORIALS_BASE_URL = __isHosted ? '/' : '/'
-
-global.isStaging = false;
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-if (!process.env.PORT) {
-    global.isDevelopment = true;
-} else {
-    if (process.env.PORT === "1446") {
-        global.isDevelopment = true;
-    } else {
-        global.isDevelopment = false;
-    }
-}
-if(process.env.CBENV && process.env.CBENV === 'STAGING'){
-    global.isStaging = true;
-}
-global.isVM = false;
-var app = require('./app')();
 
 app.set('port', process.env.PORT || 1446);
 var server = app.listen(app.get('port'), function() {
