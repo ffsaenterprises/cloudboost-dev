4488ff2a827bf149f883ada99f571ff034e705fb
ffsaenterprises <159795776+ffsaenterprises@users.noreply.github.com>
2026-03-31 08:38:12 -0500

    g

--- public/app.js
+++ public/app.js
@@ -8,18 +8,3 @@ var serverURL = null, dashboardURL = null;
 var signUpURL = '';
 var loginURL = '';
 var thisURL = '';
-
-
-if(__isDevelopment){
-	serverURL="http://localhost:3000";	
-	dashboardURL = "http://localhost:1440";
-	signUpURL="http://localhost:1440/accounts/#/signup";
-	loginURL="http://localhost:1440/accounts";
-	thisURL="http://localhost:1446";
-}else{
-	serverURL="https://cloudboost.io/api/";	
-	dashboardURL = "https://cloudboost.io/dashboard/";
-	signUpURL="https://cloudboost.io/accounts/signup";	
-	loginURL="https://cloudboost.io/accounts/";
-	thisURL="https://tutorials.cloudboost.io/";
-}
